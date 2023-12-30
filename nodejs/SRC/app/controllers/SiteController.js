18820b24cf0b37d374b3e4b9997ce35ee74bafda
KhoiNV02 <khoikakak123@gmail.com>
2023-12-30 16:36:35 +0700

    done trang Web

--- nodejs/SRC/app/controllers/SiteController.js
+++ nodejs/SRC/app/controllers/SiteController.js
@@ -2,9 +2,8 @@ const eventCurrent = require('../models/eventCurrentModel');
 const {mutipleMongooseToObject}=require('../../ultil/mongoose');
 class SiteController {
   //[get]/news
-  index(req, res) {
-    eventCurrent.find({
-    })
+  index(req, res,next) {
+    eventCurrent.find({ type: { $ne: '01' } })
    .then(event =>
     {
       res.render('home',{
@@ -12,6 +11,49 @@ class SiteController {
         showHeaderAndFooter: true,
         MSSV:req.query.MSSV,
       });
+    })
+   .catch(error => next(error));
+    // res.render('home');
+  }
+  getSchool(req,res,next)
+  {
+    eventCurrent.find({type:'1'})
+   .then(event =>
+    {
+      res.status(200).json({event});
+    })
+   .catch(error => next(error));
+    // res.render('home');
+  }
+
+  getMajor(req,res,next)
+  {
+    eventCurrent.find({type:'2'})
+   .then(event =>
+    {
+      res.status(200).json({event});
+    })
+   .catch(error => next(error));
+    // res.render('home');
+  }
+
+  getclb(req,res,next)
+  {
+    eventCurrent.find({type:'3'})
+   .then(event =>
+    {
+      res.status(200).json({event});
+    })
+   .catch(error => next(error));
+    // res.render('home');
+  }
+
+  getHot(req,res,next)
+  {
+    eventCurrent.find({ type: /0/ })
+   .then(event =>
+    {
+      res.status(200).json({event});
     })
    .catch(error => next(error));
     // res.render('home');
--- nodejs/SRC/routes/site.js
+++ nodejs/SRC/routes/site.js
@@ -2,7 +2,10 @@ var express = require('express');
 const router = express.Router();
 
 const siteController = require('../app/controllers/SiteController');
-
+router.get('/getHot', siteController.getHot);
+router.get('/getMajor', siteController.getMajor);
+router.get('/getclb', siteController.getclb);
+router.get('/getSchool', siteController.getSchool);
 router.get('/', siteController.index);
 
 module.exports = router;
